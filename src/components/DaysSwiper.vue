b931da4c74efca340e10e97bbf7fbf47c977e21a
Rozenberg-jv <avangard.npaper@gmail.com>
2021-01-29 19:00:41 +0300

    step

--- src/components/DaysSwiper.vue
+++ src/components/DaysSwiper.vue
@@ -1,5 +1,10 @@
 <template>
-  <swiper ref="mySwiper" :options="swiperOptions">
+  <swiper
+    :slides-per-view="3"
+    :space-between="50"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
     <swiper-slide>
       <DayCardHandler :cardsData="daysData['05.06.2021']" />
     </swiper-slide>
@@ -14,7 +19,7 @@
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
 import DayCardHandler from "@/components/DayCardHandler.vue";
 // import "swiper/swiper.css";
 
@@ -25,15 +30,7 @@ export default {
     Swiper,
     SwiperSlide,
   },
-  data() {
-    return {
-      swiperOptions: {
-        pagination: {
-          el: ".swiper-pagination",
-        },
-      },
-    };
-  },
+  data() {},
   methods: {
     onSwiper(swiper) {
       console.log(swiper);
@@ -43,15 +40,8 @@ export default {
     },
   },
   props: ["daysData"],
-  computed: {
-    swiper() {
-      return this.$refs.mySwiper.$swiper;
-    },
-  },
-  mounted() {
-    console.log(this.swiper);
-    this.swiper.slideTo(1, 1000, false);
-  },
+  computed: {},
+  mounted() {},
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from "vue";
+// import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
-import moment from "moment";
+// import moment from "moment";
 
-Vue.config.productionTip = false;
-Vue.prototype.$moment = moment;
+// Vue.config.productionTip = false;
+// Vue.prototype.$moment = moment;
 
-new Vue({
-  render: (h) => h(App)
-}).$mount("#app");
+createApp(App).mount("#app");
+// new Vue({
+//   render: (h) => h(App)
+// }).$mount("#app");
